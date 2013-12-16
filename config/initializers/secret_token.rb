54dc622e919e955a634bccd528d7f2a94663e913
Kennedy Ramirez <kramirezchicagotechacademy.org>
2013-12-16 14:59:34 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogBlog::Application.config.secret_token = '896dbcf09facb2629b0e4d2ac3e3d6aa614b8a4e93b4ad999b40d375aaba46253e448720d73e3646206c17c85203eb41d7c87296e54ad171f598c7583923629d'
